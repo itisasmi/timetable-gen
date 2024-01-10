3ee0c1caee3e05cb603a461d45cf38384a5d2fde
Asmi Krishnatrey <136878658+itisasmi@users.noreply.github.com>
2024-01-10 16:52:43 +0530

    Add files via upload

---
+++ Timetable.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+public class Timetable{
+    private String name;
+    String a[][] = new String[6][7];  
+    void setName(String s){
+        this.name=s;
+    } 
+    String getName(){
+        return this.name;
+    }
+}
